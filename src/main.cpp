05fdc99af8f37b768834b58d9a45cc0122c46bb1
Darlene-13 <darlenenasimiyu@gmail.com>
2026-04-23 12:05:58 +0300

    added process loop

--- src/main.cpp
+++ src/main.cpp
@@ -67,20 +67,54 @@ void evaluateThresholds(){
 }
 
 
+// Method to update LED states based on alert level
+void updateLeds(){
+    // Turn of all of them at first
+    digitalWrite(PIN_LED_GREEN, LOW);
+    digitalWrite(PIN_LED_YELLOW, LOW);
+    digitalWrite(PIN_LED_RED, LOW);
+    digitalWrite(PIN_BUZZER, LOW);
+
+    // Turn on the right LED and buzzer based on the alert level
+    if (currentAlert == DANGER){
+        digitalWrite(PIN_LED_RED, HIGH);
+        digitalWrite(PIN_BUZZER, HIGH);
+    }else if (currentAlert == WARNING){
+        digitalWrite(PIN_LED_YELLOW, HIGH);
+    }else {
+        digitalWrite(PIN_LED_GREEN, HIGH);
+    }
+}
+
+void printStatus() {
+  Serial.println("===============================================");
+  Serial.print("Pressure:    "); Serial.print(currentPressure);    Serial.println(" MPa");
+  Serial.print("Convergence: "); Serial.print(currentDisplacement); Serial.println(" mm");
+  Serial.print("Alert:       ");
+  if (currentAlert == DANGER)       Serial.println("DANGER!!! ");
+  else if (currentAlert == WARNING) Serial.println("WARNING!!");
+  else                              Serial.println("SAFE.");
+}
+
+
 // Process loop
 void loop(){
 
     // Read sensor values
-
+    int rawPressure = analogRead(PIN_PRESSURE);
+    int rawDisplacement = analogRead(PIN_DISPLACEMENT);
     //Convert to real units
-
+    currentPressure = (rawPressure / 4095.0) * 30.0; 
+    currentDisplacement = (rawDisplacement / 4095.0) * 50.0; 
     
     //Determine alert level based on thresholds
     evaluateThresholds();
 
     // Update outputs based on the alert level
+    updateLeds();
 
     // For debugging: print current sensor values and alert level
+    printStatus();
 
     delay(1000); // Delay for a while before the next reading
 
